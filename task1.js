e3016d57614fece45388c26493067d0adfa0afc7
savchenko-maksym <maxim.savchenko91@gmail.com>
2024-12-07 14:47:21 +0200

    add-first-task

--- task1.js
+++ task1.js
@@ -1,8 +1,12 @@
 // Написати функцію findFirstOccurrence, яка приймає два аргументи: рядок str і підрядок substr.
 //  Функція повинна повертати індекс першого входження підрядка в рядок.
-//  Якщо підрядок не знайдено, функція повинна повертати - 1.
+//  Якщо підрядок не знайдено, функція повинна повертати -1.
 
 // Приклади використання:
-console.log(findFirstOccurrence('Hello, world!', 'world')) // 7
-console.log(findFirstOccurrence('Hello, world!', 'JavaScript')) // -1
-console.log(findFirstOccurrence('Hello, world!', 'Hello')) // 0
+console.log(findFirstOccurrence("Hello, world!", "world")); // 7
+console.log(findFirstOccurrence("Hello, world!", "JavaScript")); // -1
+console.log(findFirstOccurrence("Hello, world!", "Hello")); // 0
+
+function findFirstOccurrence(str, substr) {
+  return str.indexOf(substr);
+}
